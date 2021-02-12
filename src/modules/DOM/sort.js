e28785ee22a42ab1e9ac34f120ea0485a767bf85
Riley Loudon <rileyloudon@icloud.com>
2021-02-11 23:50:51 -0600

    remove highest priority option

--- src/modules/DOM/sort.js
+++ src/modules/DOM/sort.js
@@ -5,7 +5,6 @@ const sort = () => {
         <h3>Sort by</h3>
         <li><a href="#" id='date-added'>Date Added</a></li>
         <li><a href="#" id='coming-up'>Coming Up</a></li>
-        <li><a href="#" id='priority'>Highest Priority</a></li>
       </ul>
     </nav>`;
 };
