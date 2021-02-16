d9a2fccaffcf9453fd27a2dceac80c160f950005
Riley Loudon <rileyloudon@icloud.com>
2021-02-16 00:16:33 -0600

    check the todo date is in the future

--- src/modules/controllers/valid-todo.js
+++ src/modules/controllers/valid-todo.js
@@ -1,11 +1,17 @@
+import { isAfter, parseISO } from 'date-fns';
 import createTodo from './create-todo';
 
 const validTodo = (e) => {
   e.preventDefault();
 
-  if (document.getElementById('todo-title').value !== '') {
-    createTodo();
-  }
+  let testDate = document.getElementById('todo-date').value;
+  let compareDate = testDate.replace(/\/|\./g, '-');
+
+  document.getElementById('todo-title').value !== ''
+    ? isAfter(parseISO(compareDate), new Date()) || testDate === ''
+      ? createTodo()
+      : alert(`You can't complete a todo in the past!`)
+    : alert('Please enter a title');
 };
 
 export default validTodo;
