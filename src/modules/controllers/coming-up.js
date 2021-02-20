c9e4062407e06ef94dfeb6ddc46a59a0fbecfbb4
Riley Loudon <rileyloudon@icloud.com>
2021-02-19 21:13:59 -0600

    sort by closest to due date

---
+++ src/modules/controllers/coming-up.js
@@ -0,0 +1,29 @@
+import renderTodo from '../DOM/render-todo';
+import { mySort, myTodos } from './user-data';
+
+const comingUp = () => {
+  const normalSort = () => {
+    myTodos = myTodos
+      .filter((e) => e.dueDate === '∞')
+      .concat(
+        myTodos
+          .filter((e) => e.dueDate !== '∞')
+          .sort((a, b) => a.dueDate - b.dueDate),
+      );
+    mySort = 'coming-up';
+    renderTodo();
+  };
+
+  const reverseSort = () => {
+    myTodos = myTodos
+      .filter((e) => e.dueDate !== '∞')
+      .sort((a, b) => b.dueDate - a.dueDate)
+      .concat(myTodos.filter((e) => e.dueDate === '∞'));
+    mySort = 'coming-up-reverse';
+    renderTodo();
+  };
+
+  mySort === 'coming-up' ? reverseSort() : normalSort();
+};
+
+export default comingUp;
