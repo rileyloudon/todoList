ec01e8316295808e55c0ad5992fdb1394b68a2f8
Riley Loudon <rileyloudon@icloud.com>
2021-02-23 23:35:46 -0600

    finish styling created groups

--- src/modules/DOM/render-groups.js
+++ src/modules/DOM/render-groups.js
@@ -6,8 +6,9 @@ const renderGroups = () => {
       (group) => `
       <div id='render-group' style='border: 1px solid ${group.groupColor}'>
         <p id='render-group-title'>${group.groupName}</p>
-        <label for='render-group-color'>Change Color: </label>
+        <label for='render-group-color' style='color:${group.groupColor}; border:1px solid ${group.groupColor}'>Change Color</label>
         <input type='color' id='render-group-color' value='${group.groupColor}'>
+        <button id='delete-group'>Delete Group</button>
       </div>`,
     )
     .join('')}`;
