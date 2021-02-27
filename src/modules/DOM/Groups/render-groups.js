a9781ec292a18d9d44c3eca003bb57bdccf2e258
Riley Loudon <rileyloudon@icloud.com>
2021-02-26 19:52:07 -0600

    add event lisenters to the groups when displayed

--- src/modules/DOM/Groups/render-groups.js
+++ src/modules/DOM/Groups/render-groups.js
@@ -1,3 +1,4 @@
+import deleteGroup from '../../controllers/Groups/delete-group';
 import newColor from '../../controllers/Groups/group-change-color';
 import { myGroups } from '../../controllers/user-data';
 
@@ -8,18 +9,27 @@ const renderGroups = () => {
     .map(
       (group) => `
       <div class='render-group' style='border: 1px solid ${group.groupColor}'>
-        <p class='render-group-title'>${group.groupName}</p>
-        <label for='${group.groupName}-group-color' style='color:${group.groupColor}; border:1px solid ${group.groupColor}'>Change Color</label>
-        <input type='color' id='${group.groupName}-group-color' class='${group.groupName} change-color' value='${group.groupColor}'>
-        <button class='delete-group'>Delete Group</button>
+        <p class='render-group-title'>${group.humanizeGroupName()}</p>
+        <label for='${group.groupName}-group-color' style='color:${
+        group.groupColor
+      }; border:1px solid ${group.groupColor}'>Change Color</label>
+        <input type='color' id='${group.groupName}-group-color' class='${
+        group.groupName
+      } change-color' value='${group.groupColor}'>
+        <button class='delete-group ${group.groupName}'>Delete Group</button>
       </div>`,
     )
     .join('')}`;
 
-  const changeColor = document.querySelectorAll('.change-color');
+  const changeColorBtn = document.querySelectorAll('.change-color');
 
-  changeColor.forEach((group) => {
-    group.addEventListener('change', () => newColor(group));
+  changeColorBtn.forEach((btn) => {
+    btn.addEventListener('change', () => newColor(btn));
+  });
+
+  const deleteGroupBtn = document.querySelectorAll('.delete-group');
+  deleteGroupBtn.forEach((btn) => {
+    btn.addEventListener('click', () => deleteGroup(btn));
   });
 };
 
