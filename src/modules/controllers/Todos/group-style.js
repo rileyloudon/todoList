202b37cf0b0168c233f1ad4af45c91be7346d23a
Riley Loudon <rileyloudon@icloud.com>
2021-03-03 21:32:52 -0600

    use users prefered color scheme

--- src/modules/controllers/Todos/group-style.js
+++ src/modules/controllers/Todos/group-style.js
@@ -1,7 +1,9 @@
 import { myGroups } from '../user-data';
 
 const groupStyle = (todo) => {
-  let color = '#eee';
+  let color = window.matchMedia('(prefers-color-scheme: light)').matches
+    ? 'var(--light-main-color)'
+    : 'var(--dark-main-color)';
   myGroups.forEach((group) => {
     if (todo.group === group.groupName) color = group.groupColor;
   });
