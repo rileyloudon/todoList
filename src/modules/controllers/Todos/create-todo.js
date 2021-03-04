c93bbe1f4d113adda0765f4614c7036113e973f8
Riley Loudon <rileyloudon@icloud.com>
2021-03-03 22:14:36 -0600

    create prototypes

--- src/modules/controllers/Todos/create-todo.js
+++ src/modules/controllers/Todos/create-todo.js
@@ -3,19 +3,53 @@ import renderTodo from '../../DOM/Todos/render-todo';
 
 const createTodo = () => {
   class Todo {
-    constructor(title, dateAdded, dueDate, group) {
-      this.title = title;
+    constructor(name, dateAdded, dueDate, group) {
+      this.name = name;
       this.dateAdded = dateAdded;
       this.dueDate = dueDate;
       this.group = group;
     }
+
+    completeTodo() {
+      myTodos = myTodos.filter((e) => e !== this);
+    }
+
+    humanizeTodoName() {
+      return this.name
+        .replace(/%20/g, ' ')
+        .replace(/%27/g, "'")
+        .replace(/(\b[a-z](?=[a-z]{2})|^[a-z])/g, (letter) =>
+          letter.toUpperCase(),
+        );
+    }
+
+    humanizeTodoGroup() {
+      return this.group
+        .replace(/%20/g, ' ')
+        .replace(/%27/g, "'")
+        .replace(/(\b[a-z](?=[a-z]{2})|^[a-z])/g, (letter) =>
+          letter.toUpperCase(),
+        );
+    }
   }
 
   let todoName = document.getElementById('todo-title').value;
+  todoName = todoName
+    .replace(/\s+/g, '%20')
+    .replace(/[']+/g, '%27')
+    .toLowerCase();
+
   let dateAdded = new Date();
+
   let todoDueDate = document.getElementById('todo-date').value;
   let todoDueTime = ' 00:00';
+
   let todoGroup = document.getElementById('todo-group').value;
+
+  todoGroup = todoGroup
+    .replace(/\s+/g, '%20')
+    .replace(/[']+/g, '%27')
+    .toLowerCase();
 
   const todo = new Todo(
     todoName,
