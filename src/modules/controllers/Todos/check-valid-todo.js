316697ffe0288564920cefb0406f4b66cc95d5ba
Riley Loudon <rileyloudon@icloud.com>
2021-03-06 01:28:11 -0600

    check for duplicates, pass errors to handler

--- src/modules/controllers/Todos/check-valid-todo.js
+++ src/modules/controllers/Todos/check-valid-todo.js
@@ -1,17 +1,38 @@
 import { isAfter, parseISO } from 'date-fns';
+import todoFormError from '../../DOM/Todos/todo-form-error';
+import { myTodos } from '../user-data';
 import createTodo from './create-todo';
 
 const validTodo = (e) => {
   e.preventDefault();
 
-  let testDate = document.getElementById('todo-date').value;
-  let compareDate = testDate.replace(/\/|\./g, '-');
+  const todoName = document.getElementById('todo-title').value;
 
-  document.getElementById('todo-title').value !== ''
+  const testDate = document.getElementById('todo-date').value;
+  const compareDate = testDate.replace(/\/|\./g, '-');
+
+  const testTime = document.getElementById('todo-time').value;
+  const timeRegex = new RegExp(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/gm);
+
+  const errorBox = document.getElementById('todo-form-error');
+
+  let alreadyExists = false;
+
+  myTodos.forEach((todo) => {
+    if (todoName === todo.name || todoName === todo.humanizeTodoName()) {
+      alreadyExists = true;
+    }
+  });
+
+  todoName !== ''
     ? isAfter(parseISO(compareDate), new Date()) || testDate === ''
-      ? createTodo()
-      : alert(`You can't complete a todo in the past!`)
-    : alert('Please enter a title');
+      ? timeRegex.test(testTime) || testTime === ''
+        ? !alreadyExists
+          ? (createTodo(), (errorBox.style.display = 'none'))
+          : todoFormError('exists')
+        : todoFormError('time')
+      : todoFormError('date')
+    : todoFormError('title');
 };
 
 export default validTodo;
