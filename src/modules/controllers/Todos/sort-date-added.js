e792f638884cf9d2e8d4c7d3dea1355576d2eacb
Riley Loudon <rileyloudon@icloud.com>
2021-03-09 22:54:58 -0600

    reverse first click

--- src/modules/controllers/Todos/sort-date-added.js
+++ src/modules/controllers/Todos/sort-date-added.js
@@ -3,13 +3,13 @@ import { mySort, myTodos } from '../user-data';
 
 const dateAdded = () => {
   const normalSort = () => {
-    myTodos.sort((a, b) => b.dateAdded - a.dateAdded);
+    myTodos.sort((a, b) => a.dateAdded - b.dateAdded);
     mySort = 'date-added';
     renderTodo();
   };
 
   const reverseSort = () => {
-    myTodos.sort((a, b) => a.dateAdded - b.dateAdded);
+    myTodos.sort((a, b) => b.dateAdded - a.dateAdded);
     mySort = 'date-added-reverse';
     renderTodo();
   };
