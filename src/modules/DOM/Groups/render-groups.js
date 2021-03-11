a1d76c5b7244395aa1cbf80306637f26ef882875
Riley Loudon <rileyloudon@icloud.com>
2021-03-10 22:14:37 -0600

    create delete warning

--- src/modules/DOM/Groups/render-groups.js
+++ src/modules/DOM/Groups/render-groups.js
@@ -1,6 +1,7 @@
 import deleteGroup from '../../controllers/Groups/delete-group';
 import newColor from '../../controllers/Groups/change-group-color';
 import { myGroups } from '../../controllers/user-data';
+import cancelDelete from '../../controllers/Groups/cancel-delete-group';
 
 const renderGroups = () => {
   const displayGroups = document.getElementById('user-groups');
@@ -8,21 +9,33 @@ const renderGroups = () => {
   displayGroups.innerHTML = `${myGroups
     .map(
       (group, i) => `
-      <div class='render-group' style='border: 1px solid ${group.groupColor}'>
-        <p class='render-group-title'>${group.humanizeGroupName()}</p>
-        <label for='${i}' style='color:${group.groupColor}; border:1px solid ${
+      <div class='render-group-container ${group.groupName}'>
+        <div class='render-group' style='border: 1px solid ${group.groupColor}'>
+          <p class='render-group-title'>${group.humanizeGroupName()}</p>
+          <label for='${i}' style='color:${
         group.groupColor
-      }'>Change Color</label>
-        <input type='color' id='${i}' class='${
+      }; border:1px solid ${group.groupColor}'>Change Color</label>
+          <input type='color' id='${i}' class='${
         group.groupName
       } change-color' value='${group.groupColor}'>
-        <button class='delete-group ${group.groupName}'>Delete Group</button>
+          <button class='delete-group ${group.groupName}'>Delete Group</button>
+        </div>
+      <div class='group-warning ${
+        group.groupName
+      }' style='display: none; border: 1px solid ${
+        group.groupColor
+      }; border-top: none;
+        '>
+          <p class='warning-text'>Deleting this group will also delete its todos.</p>
+          <p class='warning-cont'>Are you sure?</p>
+          <button class='cancel ${group.groupName}'>Cancel</button>
+          <button class='delete-all ${group.groupName}'>Delete</button>
+        </div>
       </div>`,
     )
     .join('')}`;
 
   const changeColorBtn = document.querySelectorAll('.change-color');
-
   changeColorBtn.forEach((btn) => {
     btn.addEventListener('change', () => newColor(btn));
   });
