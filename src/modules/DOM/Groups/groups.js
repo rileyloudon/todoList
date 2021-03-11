edddce8e7a49fbd1b9add78a9aa8ef7c1b18c4f1
Riley Loudon <rileyloudon@icloud.com>
2021-03-10 22:15:37 -0600

    style sidebar groups

--- src/modules/DOM/Groups/groups.js
+++ src/modules/DOM/Groups/groups.js
@@ -4,15 +4,19 @@ const groups = () => {
   return `      
   <nav>
     <ul id='groups'>
-      <h4>Groups</h4>
+      <h4 id='groups-header'>Groups</h4>
       <li><button type='button' id='edit-groups' class='btn'>Edit Groups</li>
       <div id='group-options'>
-        <li><a href="#" class='group'>View All</a></li>
-        <li><a href="#" class='group'>No Group</a></li>
+        <li><a href="#" class='group-navbar view-all-todos btn default-border'>View All</a></li>
+        <li><a href="#" class='group-navbar btn default-border'>No Group</a></li>
         ${myGroups
           .map(
             (item) =>
-              `<li><a href="#" class='group'>${item.groupName}</a></li>`,
+              `<li><a href="#" class='group-navbar btn' style='border:1px solid ${
+                item.groupColor
+              }; color:${
+                item.groupColor
+              }'>${item.humanizeGroupName()}</a></li>`,
           )
           .join('')}
       </div>
