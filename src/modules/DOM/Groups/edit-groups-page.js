7bee53a258c68bb2b1fbb097261301fb43d64500
Riley Loudon <rileyloudon@icloud.com>
2021-03-16 23:20:54 -0500

    add mobile support

--- src/modules/DOM/Groups/edit-groups-page.js
+++ src/modules/DOM/Groups/edit-groups-page.js
@@ -1,15 +1,19 @@
-import back from '../../controllers/Groups/go-back';
+import back from './go-back';
 import validGroup from '../../controllers/Groups/check-valid-group';
 import createGroupForm from './groups-form';
 import groupsNavbar from './groups-navbar';
 import renderGroups from './render-groups';
+import closeNavbar from '../../controllers/close-navbar';
 
 const editGroups = () => {
+  document.getElementById('reorder').style.display = 'none';
+
   const navbar = document.getElementById('navbar');
-  navbar.innerHTML = `${groupsNavbar()}`;
+  navbar.innerHTML = ``;
 
   const main = document.getElementById('main');
   main.innerHTML = `
+    ${groupsNavbar()}
     ${createGroupForm()}
     <div id='user-groups'></div>
     `;
@@ -21,6 +25,10 @@ const editGroups = () => {
 
   const backBtn = document.getElementById('back');
   backBtn.addEventListener('click', back);
+
+  if (window.matchMedia('(max-width: 600px)').matches) {
+    closeNavbar();
+  }
 };
 
 export default editGroups;
