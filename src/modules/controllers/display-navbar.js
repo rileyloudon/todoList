29cb7fdce7d2e94b89f808ca671d9348a1f47c97
Riley Loudon <rileyloudon@icloud.com>
2021-03-17 23:03:02 -0500

    display navbar on small screens

---
+++ src/modules/controllers/display-navbar.js
@@ -0,0 +1,27 @@
+import closeNavbar from './close-navbar';
+
+const displayNavbar = () => {
+  let color = window.matchMedia('(prefers-color-scheme: light)').matches
+    ? 'var(--light-main-color)'
+    : 'var(--dark-main-color)';
+
+  const navbar = document.getElementById('navbar');
+  navbar.style.width = '152px';
+  navbar.style.borderRight = `1px solid ${color}`;
+
+  const clickOutsideNav = document.getElementById('navbar');
+
+  const outsideClick = (e) => {
+    if (
+      !clickOutsideNav.contains(e.target) &&
+      !e.target.classList.contains('material-icons')
+    ) {
+      closeNavbar();
+      document.removeEventListener('click', outsideClick);
+    }
+  };
+
+  document.addEventListener('click', outsideClick);
+};
+
+export default displayNavbar;
