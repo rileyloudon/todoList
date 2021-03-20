6742047b652ada6169a68b2fbadc466af0e54340
Riley Loudon <rileyloudon@icloud.com>
2021-03-19 21:26:14 -0500

    close navbar if it's open

--- src/modules/controllers/Groups/view-individual-group.js
+++ src/modules/controllers/Groups/view-individual-group.js
@@ -1,14 +1,17 @@
 import renderTodo from '../../DOM/Todos/render-todo';
+import closeNavbar from '../close-navbar';
 import { viewingGroup } from '../user-data';
 
 const viewGroup = (e) => {
   if (e.target.innerHTML === 'View All') {
     viewingGroup = 'View All';
     renderTodo();
-  } else if (e.target.classList.contains('group')) {
+  } else if (e.target.classList.contains('group-navbar')) {
     viewingGroup = e.target.innerHTML;
     renderTodo();
   }
+
+  if (window.matchMedia('(max-width: 600px)').matches) closeNavbar();
 };
 
 export default viewGroup;
