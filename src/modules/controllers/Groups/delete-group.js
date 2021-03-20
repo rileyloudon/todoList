b23e23dd33d051fc4d3bb628eb718c4ff9c27afb
Riley Loudon <rileyloudon@icloud.com>
2021-03-19 21:27:37 -0500

    update local storage, pass warning to function

--- src/modules/controllers/Groups/delete-group.js
+++ src/modules/controllers/Groups/delete-group.js
@@ -1,16 +1,9 @@
 import renderGroups from '../../DOM/Groups/render-groups';
+import showWarning from '../../DOM/Groups/show-delete-warning';
 import { myGroups, myTodos } from '../user-data';
 
 const deleteGroup = (item) => {
   const deletedGroup = item.classList[1];
-
-  const showWarning = () => {
-    const warningElement = document.getElementsByClassName(
-      `group-warning ${deletedGroup}`,
-    )[0];
-
-    warningElement.style.display = 'grid';
-  };
 
   myGroups.forEach((group) => {
     if (deletedGroup === group.groupName) {
@@ -18,10 +11,10 @@ const deleteGroup = (item) => {
         myTodos.length > 0 &&
         myTodos.some((todo) => deletedGroup === todo.group)
       ) {
-        showWarning();
+        showWarning(deletedGroup);
       } else group.deleteGroup(), renderGroups();
     }
   });
+  localStorage.setItem('userGroups', JSON.stringify(myGroups));
 };
-
 export default deleteGroup;
