53460f050c6bae4809108491024c0021c5f99d38
Riley Loudon <rileyloudon@icloud.com>
2021-03-24 21:09:39 -0500

    create functions that reassign myTodos or myGroups

--- src/modules/controllers/user-data.js
+++ src/modules/controllers/user-data.js
@@ -1,6 +1,27 @@
-let myTodos = [];
-let myGroups = [];
-let mySort = 'default';
-let viewingGroup = 'View All';
+export let myTodos = [];
+export let myGroups = [];
 
-export { myTodos, myGroups, mySort, viewingGroup };
+export const myTodosComingUp = () => {
+  myTodos = myTodos
+    .filter((e) => e.dueDate === '∞')
+    .concat(
+      myTodos
+        .filter((e) => e.dueDate !== '∞')
+        .sort((a, b) => a.dueDate - b.dueDate)
+    );
+};
+
+export const myTodosComingUpReverse = () => {
+  myTodos = myTodos
+    .filter((e) => e.dueDate !== '∞')
+    .sort((a, b) => b.dueDate - a.dueDate)
+    .concat(myTodos.filter((e) => e.dueDate === '∞'));
+};
+
+export const completeTodo = (todo) => {
+  myTodos = myTodos.filter((e) => e !== todo);
+};
+
+export const deleteGroup = (group) => {
+  myGroups = myGroups.filter((e) => e !== group);
+};
