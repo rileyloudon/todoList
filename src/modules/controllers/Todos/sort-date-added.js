4e822c477554c1927e9c167a45a0d1352f8a1f47
Riley Loudon <rileyloudon@icloud.com>
2021-03-26 02:54:08 -0500

    move mySort functionality inside module

--- src/modules/controllers/Todos/sort-date-added.js
+++ src/modules/controllers/Todos/sort-date-added.js
@@ -1,21 +1,24 @@
 import renderTodo from '../../DOM/Todos/render-todo';
 import closeNavbar from '../close-navbar';
-import { mySort, myTodos } from '../user-data';
+import { myTodos } from '../user-data';
+
+let dateAddedSort;
 
 const dateAdded = () => {
   const normalSort = () => {
     myTodos.sort((a, b) => a.dateAdded - b.dateAdded);
-    mySort = 'date-added';
+    dateAddedSort = 'date-added';
     renderTodo();
   };
 
   const reverseSort = () => {
     myTodos.sort((a, b) => b.dateAdded - a.dateAdded);
-    mySort = 'date-added-reverse';
+    dateAddedSort = 'date-added-reverse';
     renderTodo();
   };
 
-  mySort === 'date-added' ? reverseSort() : normalSort();
+  if (dateAddedSort === 'date-added') reverseSort();
+  else normalSort();
 
   if (window.matchMedia('(max-width: 600px)').matches) closeNavbar();
 };
